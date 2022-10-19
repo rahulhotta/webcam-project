66fe8fcc4a235c6af608b63af61aa07a42079e3a
rahulhotta <77201683+rahulhotta@users.noreply.github.com>
2022-10-19 20:53:18 +0530

    Add files via upload

--- src/components/Webcam/Webcam.jsx
+++ src/components/Webcam/Webcam.jsx
@@ -1,62 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Webcam from "react-webcam";
-import './WebCam.css'
+import "./WebCam.css";
 
-// const WebcamComponent = () => <Webcam />;
-let camWidth = 0;
+const WebcamComponent = () => <Webcam />;
 let camHeight = 0;
-if(window.innerWidth >= 800){
-    camHeight = 200;
-}else{
-    camHeight = 400;
+if (window.innerWidth >= 900) {
+  camHeight = 200;
+} else {
+  camHeight = 420;
 }
 const videoConstraints = {
-    width: 200,
-    height: camHeight,
-    facingMode: "environment"
+  width: 200,
+  height: camHeight,
+  facingMode: "environment",
 };
 
 export const WebcamCapture = () => {
+  const [image, setImage] = useState("");
+  const webcamRef = React.useRef(null);
 
-    const [image,setImage]=useState('');
-    const webcamRef = React.useRef(null);
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImage(imageSrc);
+  });
 
-    
-    const capture = React.useCallback(
-        () => {
-        const imageSrc = webcamRef.current.getScreenshot();
-        setImage(imageSrc)
-        });
-
-
-    return (
-        <div className="webcam-container">
-            <div className="webcam-img">
-
-                {image == '' ? <Webcam
-                    audio={false}
-                    width={200}
-                    height={camHeight}
-                    ref={webcamRef}
-                    screenshotFormat="image/jpeg"
-                    videoConstraints={videoConstraints}
-                /> : <img src={image} />}
-            </div>
-            <div>
-                {image != '' ?
-                    <button onClick={(e) => {
-                        e.preventDefault();
-                        setImage('')
-                    }}
-                        className="webcam-btn">
-                        Retake Image</button> :
-                    <button onClick={(e) => {
-                        e.preventDefault();
-                        capture();
-                    }}
-                        className="webcam-btn">Capture</button>
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className="webcam-container">
+      <div className="webcam-img">
+        {image === "" ? (
+          <Webcam
+            audio={false}
+            width={200}
+            height={camHeight}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+        ) : (
+          <img src={image} />
+        )}
+      </div>
+      <div>
+        {image !== "" ? (
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setImage("");
+            }}
+            className="webcam-btn"
+          >
+            Retake Image
+          </button>
+        ) : (
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              capture();
+            }}
+            className="webcam-btn"
+          >
+            Capture
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
